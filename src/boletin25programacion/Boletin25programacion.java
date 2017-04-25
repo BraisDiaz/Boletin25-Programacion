562b68bb75bb905166b64c3c9bede3a109e29675
BraisDiaz <braisdiaz@hotmail.com>
2017-04-25 09:56:58 +0200

    Boletin 25 (falta ver el switch)

---
+++ src/boletin25programacion/Boletin25programacion.java
@@ -0,0 +1,62 @@
+
+package boletin25programacion;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Brais Núñez
+ */
+public class Boletin25programacion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+       Ventana1 ven1 = new Ventana1();
+       Ventana2 ven2 = new Ventana2();
+       Ventana3 ven3 = new Ventana3();
+       Ventana4 ven4 = new Ventana4();
+       
+       ven1.setVisible(true);
+       ven2.setVisible(true);
+       ven3.setVisible(true);
+       ven4.setVisible(true);
+       
+//      int opcion;
+//    
+//      do {
+//
+//    opcion = Integer.parseInt(JOptionPane.showInputDialog("1.Ejercicio Botón  \n2.Ejercicio JFrame  \n0.Salir"));
+//
+//    switch(opcion){
+//
+//        case 1:Ventana1 ven1 = new Ventana1(); ven1.setVisible(true);
+//            
+//            break;
+//            
+//        case 2:ven2.setVisible(true);
+//
+//            break;
+//
+//        default:
+//                
+//            break;
+//    }
+//          }while(opcion!=0);   
+//    }    
+}
+}
+
+            
+            
+            
+        
+        
+        
+
+        
+        
+    
+
